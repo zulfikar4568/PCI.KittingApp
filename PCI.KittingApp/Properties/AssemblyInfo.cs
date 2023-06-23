95d988bf404bb533782ea67ed3729c08a0607310
zulfikar4568 <isnaen70@gmail.com>
2023-06-23 15:45:51 +0000

    chore(update): v1.10.0 push to the AssemblyInfo.cs

--- PCI.KittingApp/Properties/AssemblyInfo.cs
+++ PCI.KittingApp/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.9.5.54")]
-[assembly: AssemblyVersion("1.9.5.54")]
-[assembly: AssemblyFileVersion("1.9.5.54")]
+// [assembly: AssemblyVersion("1.10.0.57")]
+[assembly: AssemblyVersion("1.10.0.57")]
+[assembly: AssemblyFileVersion("1.10.0.57")]
